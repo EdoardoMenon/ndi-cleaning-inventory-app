bcd1de334d52bded3a77fe0bd3c9d59c6995c089
Edoardo Menon <edoardomenon1997@gmail.com>
2026-03-03 23:34:39 +1100

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
